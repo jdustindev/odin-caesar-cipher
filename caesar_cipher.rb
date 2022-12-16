b8a37f7ae8f3ae00e467877cea90b6f7714940af
jdustindev <jdustindev@gmail.com>
2022-12-16 15:31:01 -0500

    Fix cipher logic
    I was wrong -- it was not the lowercase chars that had a logic issue, it
    was that shift_factor was being modified. This caused shift_factor to
    change on the first 'W', leading to completely different output than
    expected.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,20 +1,22 @@
 def caesar_cipher(string, shift_factor)
     ciphered_string = string.split('').map do |char|
+        current_shift_factor = shift_factor
         new_char = char unless (char.between?('A', 'Z') || char.between?('a', 'z'))
 
         # implement cipher here
         if char.between?('A', 'Z')
-            shift_factor -= 26 if (char.ord + shift_factor).chr > 'Z'
-            new_char = (char.ord + shift_factor).chr
+            current_shift_factor -= 26 if (char.ord + shift_factor).chr > 'Z'
+            new_char = (char.ord + current_shift_factor).chr
         end
 
         if char.between?('a', 'z')
             shift_factor -= 26 if (char.ord + shift_factor).chr > 'z'
-            new_char = (char.ord + shift_factor).chr
+            new_char = (char.ord + current_shift_factor).chr
         end
 
         new_char
     end
+    ciphered_string.join('')
 end
 
 puts caesar_cipher("What a string!", 5)
